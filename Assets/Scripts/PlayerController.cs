e94217b6f8e815972c7e8d251ddd18464b307b6c
TianaY320 <63749917+TianaY320@users.noreply.github.com>
2020-09-05 00:42:15 -0400

    Player Controller Script
    Followed code but the ball won't move on my Unity screen. It compiles fine and I quadruple checked the code with theirs but so far it's not working. I will keep trying!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
 
     private Rigidbody rb;
+
     private float movementX;
     private float movementY;
 
@@ -15,17 +17,19 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    void OnMove(InputValue movementValue)
+
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement); 
-    }
 
+        rb.AddForce(movement*speed);
+    }
 }
